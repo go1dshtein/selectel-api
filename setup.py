8e3efb24d9ee729a4d1c7bc92bee1644fdaaa34a
Kirill Goldshtein <goldshtein.kirill@gmail.com>
2015-10-28 00:04:27 +0300

    next version 0.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 setup(name='selectel-api',
-      version='0.1.3',
+      version='0.1.4',
       description='Simple selectel API',
       author='Kirill Goldshtein',
       author_email='goldshtein.kirill@gmail.com',
