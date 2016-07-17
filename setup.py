a7e3c867db4c257afdf896e317356d9923e073b2
Kirill Goldshtein <goldshtein.kirill@gmail.com>
2016-07-17 22:08:57 +0300

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 setup(name='selectel-api',
-      version='0.1.4',
+      version='0.1.5',
       description='Simple selectel API',
       author='Kirill Goldshtein',
       author_email='goldshtein.kirill@gmail.com',
